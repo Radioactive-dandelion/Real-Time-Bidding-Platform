04f56d88c07035d19fe17a21784c4da2f7ceac33
polina <polina.zimina@final.edu.tr>
2026-06-04 00:40:59 +0300

    Changed pubsub.py

--- app/services/pubsub.py
+++ app/services/pubsub.py
@@ -6,25 +6,34 @@ from app.websocket.manager import manager
 
 async def redis_subscriber():
 
-    pubsub = redis_client.pubsub()
+    pubsub = redis_client.pubsub(
+        ignore_subscribe_messages=True,
+    )
 
     await pubsub.psubscribe("auction:*")
 
     print("Redis subscriber started")
 
-    async for message in pubsub.listen():
+    while True:
 
-        if message["type"] != "pmessage":
+        try:
+
+            message = await pubsub.get_message(
+                ignore_subscribe_messages=True,
+                timeout=1.0,
+            )
+
+            if message is None:
+                continue
+
+            if message["type"] != "pmessage":
+                continue
+
+            data = json.loads(message["data"])
+            auction_id = data["auction_id"]
+
+            await manager.broadcast(auction_id, data)
+
+        except Exception as e:
+            print(f"Pubsub error: {e}")
             continue
-
-        print("REDIS MESSAGE:")
-        print(message)
-
-        data = json.loads(message["data"])
-
-        auction_id = data["auction_id"]
-
-        await manager.broadcast(
-            auction_id,
-            data,
-        )
